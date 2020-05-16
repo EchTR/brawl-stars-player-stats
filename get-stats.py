533e955b7d7f159c871e7db474bf289421e20185
Efe İ <41235050+EchTR@users.noreply.github.com>
2020-05-16 19:08:05 +0300

    Update get-stats.py

--- get-stats.py
+++ get-stats.py
@@ -62,28 +62,16 @@ if __name__ == "__main__":
 """
 I am writing this comment line so that the python files are in the majority. it has no function. its purpose is not to take notes.
 
-1/k_tag = get_data(u_id)["user_tag"][:]
-2/k_tag = get_data(u_id)["user_tag"][1:],
-				k_name = get_data(u_id)["user_name"],
-				k_color = get_data(u_id)["user_color"],
-				1/k_club = get_data(u_id)["user_club"],
-				2/k_club = k_c,
-				k_trophies = get_data(u_id)["user_trophies"],
-				k_highest = get_data(u_id)["user_highest_trophies"],
-				k_level = get_data(u_id)["user_level"],
-				k_solo = get_data(u_id)["user_s_victories"],
-				k_duo = get_data(u_id)["user_d_victories"],
-				k_3 = get_data(u_id)["user_3_victories"]
-1/k_tag = get_data(u_id)["user_tag"][:]
-2/k_tag = get_data(u_id)["user_tag"][1:],
-				k_name = get_data(u_id)["user_name"],
-				k_color = get_data(u_id)["user_color"],
-				1/k_club = get_data(u_id)["user_club"],
-				2/k_club = k_c,
-				k_trophies = get_data(u_id)["user_trophies"],
-				k_highest = get_data(u_id)["user_highest_trophies"],
-				k_level = get_data(u_id)["user_level"],
-				k_solo = get_data(u_id)["user_s_victories"],
-				k_duo = get_data(u_id)["user_d_victories"],
-				k_3 = get_data(u_id)["user_3_victories"]
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id nibh commodo sem varius facilisis.
+Curabitur fermentum eu justo at viverra. Morbi id suscipit purus. Nunc laoreet nunc nisl, quis dapibus nisl tristique eu. 
+Cras quis mauris eu lectus scelerisque accumsan quis eu neque. Nam a gravida ipsum. Donec egestas felis augue, vel tempus tellus congue ut. Sed justo nunc, commodo et condimentum vel, dictum nec erat. Nulla rhoncus ut purus at condimentum. Nam mattis id nisl nec suscipit. 
+Etiam et sodales nibh. Nulla vel commodo purus, at cursus nulla. Vivamus quis sem at odio tempus consectetur. 
+Nunc bibendum tempor laoreet. In congue, velit et viverra convallis, mauris turpis luctus lorem, vel semper elit enim sit amet neque.
+Ut finibus ultrices turpis rutrum ornare. Donec vitae tristique risus. Proin tristique massa quis posuere viverra. 
+In eleifend mi nec malesuada bibendum. Aliquam felis mauris, feugiat eu lacus et, consectetur interdum risus. Nam et nunc et lacus 
+vehicula ornare. Donec pellentesque nisl et mattis euismod. Proin cursus, leo sed dignissim dignissim, est mi semper neque, vitae
+vehicula erat dui id nisl. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+Aenean commodo commodo lacus sit amet ultrices. Nam laoreet et purus in tincidunt. Phasellus a mattis erat. 
+In interdum sapien nec maximus aliquet. Donec nisi ex, vehicula ut orci id, ultricies tincidunt tortor. Morbi maximus ultrices ipsum, a
+varius urna rhoncus eget.
 """
